0301ea7e71a1fcfb477ec05d0f46891b9c76508c
Arya Saumitra <aryasaumitra@gmail.com>
2022-01-24 09:11:56 +0530

    Added Blank Images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Image } from 'antd';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function CardComponent(props){
+
+  return(
+    <Image
+    width = {200}
+    src = {require("./images/blank.png")}
+    preview = {false} 
+    />
   );
 }
 
-export default App;
+
+class BoardComponent extends React.Component{
+
+  renderImage(i){
+    return <CardComponent /> //need to add Value i based on Card Array index
+  }
+
+  render(){
+    return (
+      
+
+      <div>
+        <div className='board-row'>
+          {[0,1,2,3].map((n) => this.renderImage(n))}
+        </div>
+        <div className='board-row'>
+          {[4,5,6,7].map((n) => this.renderImage(n))}
+        </div>
+        <div className='board-row'>
+          {[8,9,10,11].map((n) => this.renderImage(n))}
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default BoardComponent;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BoardComponent from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BoardComponent/>
   </React.StrictMode>,
   document.getElementById('root')
 );
